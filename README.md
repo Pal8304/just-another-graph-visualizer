bca30fc4054fed9a1f9847301dcace3b45ec8154
nirlep5252 <hello@nirlep.dev>
2024-10-22 10:08:44 +0530

    ci: add husky pre-commit hooks

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## TODO:
 
 - [ ] User way to create graph for user
-    - [ ] Different modes such as (create node, create edge, delete node, delete edge)
+  - [ ] Different modes such as (create node, create edge, delete node, delete edge)
 - [ ] Add problems for that can be visualized on the graph
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,9 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center p-4">
         Canvas type something here
         <div className="flex flex-row justify-center w-2/3 p-4 m-4 gap-4">
-          <Button>
-            Add Node
-          </Button>
-          <Button>
-            Add Edge
-          </Button>
-          <Button>
-            Run Algorithm
-          </Button>
+          <Button>Add Node</Button>
+          <Button>Add Edge</Button>
+          <Button>Run Algorithm</Button>
         </div>
       </div>
     </div>
