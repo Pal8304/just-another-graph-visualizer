91dba8a0caf2452a5e44dbc9b260b7cef71ec409
nirlep5252 <hello@nirlep.dev>
2024-10-26 17:31:54 +0000

    refactor: graph store

--- src/lib/stores/graph.ts
+++ src/lib/stores/graph.ts
@@ -42,76 +42,56 @@ export const useGraphStore = create(
       action: "view",
       selectedNode: null,
       addEdge: (from: Node, to: Node) => {
-        set({
-          edges: [
-            ...useGraphStore.getState().edges,
-            { directed: false, from, to, weight: null },
-          ],
-        });
-        console.log("Added edge", from, to);
+        set(({ edges }) => ({
+          edges: [...edges, { directed: false, from, to, weight: null }],
+        }));
       },
       removeEdge: (from: Node, to: Node) => {
         // might have to change this to removeEdge: (edge: Edge) cause of multiple edges
-        set({
-          edges: useGraphStore.getState().edges.filter((edge) => {
-            return edge.from.id !== from.id || edge.to.id !== to.id;
-          }),
-        });
-        console.log("Remove edge", from, to);
+        set(({ edges }) => ({
+          edges: edges.filter(
+            (edge) => edge.from.id !== from.id || edge.to.id !== to.id,
+          ),
+        }));
       },
-      setSelectedNode: (node: Node) => {
-        const selectedNode = useGraphStore.getState().selectedNode;
-        if (selectedNode === node) {
-          set({ selectedNode: null });
-        } else if (selectedNode) {
-          if (useGraphStore.getState().action === "add-edge") {
-            useGraphStore.getState().addEdge(selectedNode, node);
+      setSelectedNode: (node: Node) =>
+        set(({ selectedNode, action, addEdge, removeEdge }) => {
+          if (selectedNode && selectedNode !== node) {
+            if (action === "add-edge") {
+              addEdge(selectedNode, node);
+            }
+            if (action === "remove-edge") {
+              removeEdge(selectedNode, node);
+            }
           }
-          if (useGraphStore.getState().action === "remove-edge") {
-            useGraphStore.getState().removeEdge(selectedNode, node);
-          }
-          set({ selectedNode: null });
-        } else {
-          set({ selectedNode: node });
-        }
-      },
-      addNode: (node: Node) => {
-        set({ nodes: [...useGraphStore.getState().nodes, node] });
-        console.log("Added node", node);
-      },
+          return {
+            selectedNode: selectedNode ? null : node,
+          };
+        }),
+      addNode: (node: Node) =>
+        set({ nodes: [...useGraphStore.getState().nodes, node] }),
       removeNode: (node: Node) => {
-        set({
-          nodes: useGraphStore.getState().nodes.filter((n) => n.id !== node.id),
-        });
-        set({
-          edges: useGraphStore
-            .getState()
-            .edges.filter(
-              (edge) => edge.from.id !== node.id && edge.to.id !== node.id,
-            ),
-        });
-        console.log("Removed node", node);
+        set(({ nodes, edges }) => ({
+          nodes: nodes.filter((n) => n.id !== node.id),
+          edges: edges.filter(
+            (edge) => edge.from.id !== node.id && edge.to.id !== node.id,
+          ),
+        }));
       },
       setAction: (action: State["action"]) => {
         set({ action: action });
       },
       updateNodePosition: (id: string, x: number, y: number) => {
-        set({
-          nodes: useGraphStore
-            .getState()
-            .nodes.map((node) => (node.id === id ? { ...node, x, y } : node)),
-        });
-        set({
-          edges: useGraphStore.getState().edges.map((edge) => {
-            if (edge.from.id === id) {
-              return { ...edge, from: { ...edge.from, x, y } };
-            }
-            if (edge.to.id === id) {
-              return { ...edge, to: { ...edge.to, x, y } };
-            }
-            return edge;
-          }),
-        });
+        set(({ nodes, edges }) => ({
+          nodes: nodes.map((node) =>
+            node.id === id ? { ...node, x, y } : node,
+          ),
+          edges: edges.map((edge) => ({
+            ...edge,
+            from: edge.from.id === id ? { ...edge.from, x, y } : edge.from,
+            to: edge.to.id === id ? { ...edge.to, x, y } : edge.to,
+          })),
+        }));
       },
     }),
     {
